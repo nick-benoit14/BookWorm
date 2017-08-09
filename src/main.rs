98b2ec72cef828ac647774b47d5fba5183696b20
Nick Benoit <nick.benoit14@gmail.com>
2017-08-09 16:28:00 -0600

    add prepared statement

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+// QUESTION: Is there a good way to create default struct
+//   Yes implement the default trait
+// https://stackoverflow.com/questions/19650265/is-there-a-faster-shorter-way-to-initialize-variables-in-a-rust-struct
 
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
@@ -10,11 +13,11 @@ extern crate serde;
 
 use rocket_contrib::{Json, Value};
 
-#[cfg(test)] mod tests;
+//#[cfg(test)] mod tests; // TODO add tests
 
 pub mod Db {
   extern crate postgres;
-  use self::postgres::{Connection, TlsMode};
+  pub use self::postgres::{Connection, TlsMode};
   pub fn get_connection() -> Connection{
    let conn = Connection::connect(
        "postgres://nickbenoit@localhost:5432/book_worm_dev",
@@ -28,9 +31,26 @@ pub mod person {
 
   #[derive(Serialize, Deserialize)]
   pub struct Person {
-    pub id: i32,
+    pub id: Option<i32>,
     pub name: String
   }
+
+  impl Default for Person {
+    fn default() -> Person {
+      Person { id: None, name: String::new() }
+    }
+  }
+
+  //TODO figure out how to update id and return to create_dependencies
+ pub fn create(person: &Person) -> Person {
+   let conn = Db::get_connection();
+   let result = conn.execute(
+      "INSERT INTO people  (name) VALUES ($1)",
+      &[&person.name]
+    ).unwrap();
+   print!(result);
+   Person { ..Default::default() } //TODO
+ }
 }
 
 pub mod book {
@@ -38,31 +58,57 @@ pub mod book {
 
   #[derive(Serialize, Deserialize)]
   pub struct Book {
-    pub id: i32,
+    pub id: Option<i32>,
     pub title: String
+  }
+
+  pub fn create(book: &Book) -> Book {
+   let conn = Db::get_connection();
+   let result = conn.execute(
+      "INSERT INTO book  (title) VALUES ($1)",
+      &[&book.title]
+    ).unwrap();
+
+   //TODO use prepared statemetns
+   let stmt = try!(conn.prepare("INSERT INTO foo (bar) VALUES ('baz') RETURNING id"));
+   let id: i32 = try!(stmt.query(&[])).iter().next().unwrap().get(0);
+   // TODO
+   Book { id: Some(0), title: String::from("") }
   }
 }
 
 pub mod interaction {
   pub use Db;
-  pub use person::{Person};
-  pub use book::{Book};
+  pub use person;
+  pub use book;
 
   #[derive(Serialize, Deserialize)]
   pub struct Interaction {
     id: Option<i32>,
-    book: Book,
-    person: Person,
+    book: book::Book,
+    person: person::Person,
     comment: String,
+  }
+
+  //TODO return new interaction
+  fn create_dependants<'a>(conn: &'a Db::Connection, interaction: &'a Interaction) -> &'a Interaction {
+  if interaction.book.id.is_none() {
+    let book = book::create(&interaction.book);
+   }
+
+//   if interaction.person.id.is_none() {
+//     interaction.person = person::create(&interaction.person);
+//   }
+   interaction
   }
 
 
   pub fn create(interaction: Interaction) -> Interaction{
    let conn = Db::get_connection();
-//   let result = conn.execute(
-//      "INSERT INTO interactions  (book_id, person_id, message) VALUES ($1, $2, $3)",
-//      &[&interaction.book_id, &me.data]
-//    ).unwrap();
+   let result = conn.execute(
+      "INSERT INTO interactions  (book_id, person_id, comment) VALUES ($1, $2, $3)",
+      &[&interaction.book.id, &interaction.person.id, &interaction.comment]
+    ).unwrap();
 
     interaction
   }
@@ -78,8 +124,8 @@ pub mod interaction {
      ).unwrap() {
         let interaction  = Interaction {
             id: row.get(0),
-            book: Book {id: row.get(4), title: row.get(5)},
-            person: Person {id: row.get(6), name: row.get(7)},
+            book: book::Book {id: row.get(4), title: row.get(5)},
+            person: person::Person {id: row.get(6), name: row.get(7)},
             comment: row.get(3)
         };
         result.push(interaction);
