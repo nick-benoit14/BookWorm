4a487fdf9fa8fada41c06b5b393df09fa6f82f5a
Nick Benoit <nick.benoit14@gmail.com>
2017-09-13 19:33:25 -0600

    update db

--- db/test.sql
+++ db/test.sql
@@ -1,3 +1,5 @@
 select * from interactions i
   INNER JOIN books on books.id=i.book_id
-  INNER JOIN people on people.id=i.person_id;
+  INNER JOIN people on people.id=i.person_id
+  WHERE i.id=1 LIMIT 1
+
--- db/test_data.sql
+++ db/test_data.sql
@@ -1,3 +1,3 @@
 INSERT INTO people (name) VALUES('NB');
 insert into books(title) values ('Moby Dick');
-INSERT INTO interactions(book_id, person_id, comment) VALUES(1,1,'meh');
+INSERT INTO interactions(book_id, person_id, comment, approved) VALUES(1,1,'meh', true);
